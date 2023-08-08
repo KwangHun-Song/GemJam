f9b11be7d0ee608eb09d53274c313866def9fce0
ghyoon <ghyoon@puzzleone.com>
2023-08-08 23:21:45 +0900

    Update  | View에 UI 리소스 적용

--- program/Assets/Scripts/GemMatch/View/EntityView/NormalPieceView.cs
+++ program/Assets/Scripts/GemMatch/View/EntityView/NormalPieceView.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,12 +7,30 @@ namespace GemMatch {
     public class NormalPieceView : EntityView, IReceiveActivation {
         [Header("빨, 주, 노, 초, 파, 보 순서로 넣어주세요.")]
         [SerializeField] private Sprite[] sprites;
+        [SerializeField] private Sprite[] dimSprites;
+        [SerializeField] private Sprite[] slotSprites;
         [SerializeField] private Image mainImage;
+        [SerializeField] private Image dimImage;
+        [SerializeField] private Image slotImage;
 
         public async override UniTask OnCreate() {
-            if (Entity.Color > ColorIndex.Cyan || Entity.Color < 0) return;
+            if (Constants.UsableColors.Contains(Entity.Color)) return;
             mainImage.sprite = sprites[(int)Entity.Color];
-            mainImage.color = Color.gray;
+            dimImage.sprite = dimSprites[(int)Entity.Color];
+            slotImage.sprite = slotSprites[(int)Entity.Color];
+            SetForSlot(false);
+            SetClickable(false);
+        }
+
+        public void SetClickable(bool isClickable) {
+            mainImage.enabled = isClickable;
+            dimImage.enabled = !isClickable;
+        }
+
+        public void SetForSlot(bool isForSlot) {
+            mainImage.enabled = !isForSlot;
+            dimImage.enabled = !isForSlot;
+            slotImage.enabled = isForSlot;
         }
 
         public void OnClick() {
@@ -19,7 +38,8 @@ namespace GemMatch {
         }
 
         public async UniTask OnActive() {
-            mainImage.color = Color.white;
+            SetForSlot(false);
+            SetClickable(true);
         }
     }
 }
