46009edb8749fdd73b34b5a9ceea2d8c7b389fcd
Hongae <dahong@puzzleone.com>
2023-08-09 18:41:28 +0900

    백그라운드 전송 광훈님께 스크립트 부탁드림

---
+++ program/Assets/Effects/Script/UVStreamController02.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UVStreamController02 : MonoBehaviour
+{
+
+    public float xoffsetStart = 0.0f;
+    public float xoffsetEnd = 1.0f;
+    public float yoffsetStart = 0.0f;
+    public float yoffsetEnd = 1.0f;
+    public float xSpeedDefault = 1.0f;
+    public float ySpeedDefault = 1.0f;
+    public float RotationSetting = 0.0f;
+    public float delayTime = 1.0f;
+
+    public string TextureName;
+    float xOffset = 0.0f;
+    float yOffset = 0.0f;
+    float rotationSet = 0.0f;
+
+    public float refStrength = 0;
+
+    Image image;  
+
+    void Awake()
+    {        
+        image = GetComponent<Image>();
+        image.material = Instantiate<Material>(image.material);       
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(TintStream());
+    }  
+
+    void OnDisable()
+    {
+        
+    }
+
+    private IEnumerator TintStream ()
+    {
+        while(true)
+        {
+            // TODO: 시작과 끝은 Start, End 로 표현한다.
+            // TODO: 속도는 무조건 양수로 적는다.(음수로 적을 경우 절대값 사용)
+
+            yOffset = yoffsetStart;
+            xOffset = xoffsetStart;
+            float rotationSet = RotationSetting;
+            var xFinished = false;
+            var yFinished = false;
+            //var rotationFinished = false;
+            //if (RotationSpeed == 0) {
+                //rotationSet = 0.0F;
+            //    rotationFinished = true;
+            //} 
+            if (ySpeedDefault == 0) {
+                yFinished = true;
+            }
+            if (xSpeedDefault == 0) {
+                xFinished = true;
+            }
+            var xSpeed = Mathf.Abs(xSpeedDefault);
+            if (xoffsetStart > xoffsetEnd) {
+                xSpeed *= -1;
+            }
+            var ySpeed = Mathf.Abs(ySpeedDefault);
+            if (yoffsetStart > yoffsetEnd) {
+                ySpeed *= -1;
+            }
+
+            
+            while (xFinished == false || yFinished == false) {
+                if (yOffset * ySpeed > yoffsetEnd * ySpeed) {
+                    yFinished = true;
+                }
+                
+                if (xOffset * xSpeed > xoffsetEnd * xSpeed) {
+                    xFinished = true;
+                }
+
+                //if (rotationSet >= -Mathf.PI && rotationSet <= Mathf.PI) {
+                //  rotationFinished = true;
+                //}
+            
+
+                image.material.SetTextureOffset(TextureName, new Vector2(xOffset, yOffset));
+                image.material.SetFloat("_UVRotation",rotationSet);
+                image.material.SetFloat("_RefStrength",refStrength);
+                
+                if (xFinished == false) xOffset += Time.deltaTime * xSpeed;
+                if (yFinished == false) yOffset += Time.deltaTime * ySpeed;                 
+                //if (rotationFinished == false) rotationSet += Time.deltaTime;
+        
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(delayTime);
+        }
+        
+
+        // else
+        // {
+        //     while(true)
+        //     {
+        //         xOffset = 0;
+        //         yOffset = 0;
+        
+        //         while(rotationSet >= -Mathf.PI && rotationSet <= Mathf.PI)
+        //         {
+        //             image.material.SetTextureOffset(TextureName, new Vector2(xOffset, yOffset));
+        //             image.material.SetFloat("_UVRotation",rotationSet);                    
+        //             rotationSet += Time.deltaTime * RotationSpeed;
+                
+        //             yield return null;
+        //         }         
+
+        //     yield return new WaitForSeconds(delayTime);
+        //     }
+        // }               
+    }
+    
+}
---
+++ program/Assets/Effects/Script/UVStreamMain.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UVStreamMain : MonoBehaviour {
+
+    public float XSpeed = 0.5f; //X 방향 물결치는 속도
+    public float YSpeed = 0.0f; //Y 방향 물결치는 속도
+    float XOffset;
+    float YOffset;
+
+    Renderer _rend;
+    Renderer rend {
+        get
+        {
+            if (_rend == null)
+            {
+                _rend = GetComponent<Renderer>();
+                _rend.material = Instantiate<Material>(rend.material);
+                _rend.material.GetTextureOffset(TextureName);
+            }
+            return _rend;
+        }
+    }
+
+    public string TextureName;
+    public float interval = 1.0f;
+
+    [Header("Tilling")]
+    public bool mapTillingToScale = false;
+    public Transform scaleParent;
+
+    void OnEnable()
+    {
+//        StartCoroutine(UVStream());
+//        StartCoroutine(CoMapTillingToScale());
+    }	
+
+    IEnumerator UVStream() {        
+        while(gameObject.activeSelf) {
+            while(XSpeed >= 0f ? XOffset < 1f : XOffset > -1f) {
+                rend.material.SetTextureOffset(TextureName,new Vector2(XOffset,YOffset));
+                
+                XOffset += XSpeed * Time.deltaTime;
+                YOffset += YSpeed * Time.deltaTime;
+                yield return null;                
+            }
+            XOffset = XOffset > 1f ? XOffset - 1f : XOffset;
+            XOffset = XOffset < -1f ? XOffset + 1f : XOffset;
+            YOffset = YOffset > 1f ? YOffset - 1f : YOffset;
+            YOffset = YOffset < 0f ? YOffset + 1f : YOffset;        
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    IEnumerator CoMapTillingToScale() {
+        while (mapTillingToScale && gameObject.activeSelf) {
+            Vector3 parentScale = scaleParent.localScale;
+            rend.material.SetTextureScale(TextureName, new Vector2(parentScale.x, parentScale.y));
+            yield return null;
+        }
+    }
+}
