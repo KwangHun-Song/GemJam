73f631df16c82bc816757c22cb15f5c4f6a3abe0
ghyoon <ghyoon@puzzleone.com>
2023-08-10 17:34:16 +0900

    Update | 경계없애는 로직

--- program/Assets/Scripts/GemMatch/View/TileView.cs
+++ program/Assets/Scripts/GemMatch/View/TileView.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +9,9 @@ namespace GemMatch {
         [SerializeField] private Image background;
         [SerializeField] public Transform entitiesRoot;
         [SerializeField] public Transform guestRoom;
+        [SerializeField] public Sprite[] BackgroundSprites;
+        [SerializeField] public GameObject[] edges; // up, down, left, right
+        [SerializeField] public GameObject[] points; // LU, LD, RU, RD
         
         public Tile Tile { get; private set; }
         public View View { get; private set; }
@@ -46,7 +51,42 @@ namespace GemMatch {
         }
 
         public void Redraw() {
-            background.color = Tile.Model.IsOpened == false ? Color.gray : Color.white;
+            // background.color = Tile.Model.IsOpened == false ? Color.gray : Color.white;
+            background.sprite = Tile.Model.IsOpened ? BackgroundSprites[0] : BackgroundSprites[1];
         }
+
+        public void RedrawEdges(Func<Tile, Tile[], IEnumerable<Tile>> adjacentTilesCall, Tile[] controllerTiles) {
+            if (this.Tile.IsOpened) {
+                foreach (var edge in edges.Concat(points)) {
+                    edge.SetActive(false);
+                }
+                return;
+            }
+            var adjacents = adjacentTilesCall.Invoke(Tile, controllerTiles);
+            var direction = Enumerable.Repeat(false, 4).ToArray(); // up, down, left, right
+            var up = direction[0];
+            var down = direction[1];
+            var left = direction[2];
+            var right = direction[3];
+            foreach (var neighbor in adjacents) {
+                if (neighbor.X == Tile.X) {
+                    up |= neighbor.Y > Tile.Y && neighbor.IsOpened == false;
+                    down |= neighbor.Y < Tile.Y && neighbor.IsOpened == false;
+                } else if (neighbor.Y == Tile.Y) {
+                    left |= neighbor.X < Tile.X && neighbor.IsOpened == false;
+                    right |= neighbor.X > Tile.X && neighbor.IsOpened == false;
+                }
+            }
+
+            edges[0].SetActive(up);
+            edges[1].SetActive(down);
+            edges[2].SetActive(left);
+            edges[3].SetActive(right);
+            points[0].SetActive(up && left);
+            points[0].SetActive(down && left);
+            points[0].SetActive(up && right);
+            points[0].SetActive(up && right);
+        }
+
     }
 }
--- program/Assets/Scripts/GemMatch/View/View.cs
+++ program/Assets/Scripts/GemMatch/View/View.cs
@@ -8,8 +8,7 @@ namespace GemMatch {
     public class View : MonoBehaviour, IControllerEvent {
         [SerializeField] private Transform tileViewRoot;
         [SerializeField] private Transform memoryViewRoot;
-        [SerializeField] private TMP_Text gameStatusText;
-        
+
         [Header("MonoBehaviour를 상속한 AbilityView들은 여기에!")]
         [SerializeField] private ShuffleAbilityView shuffleAbilityView;
 
@@ -36,6 +35,7 @@ namespace GemMatch {
             for (int i = 0; i < TileViews.Length; i++) {
                 TileViews[i].Initialize(this, tiles[i]);
             }
+            RedrawEdges();
 
             foreach (var tileView in TileViews) {
                 foreach (var entityView in tileView.EntityViews.Values) {
@@ -46,16 +46,14 @@ namespace GemMatch {
             foreach (var memoryView in MemoryViews) {
                 memoryView.Initialize();
             }
-
-            gameStatusText.text = "";
         }
 
         public void OnClearGame(Mission[] missions) {
-            gameStatusText.text = "Completed!";
+            // todo: 성공팝업
         }
 
         public void OnFailGame(Mission[] missions) {
-            gameStatusText.text = "failed!";
+            // todo: 실패팝업
         }
 
         public void OnReplayGame(Mission[] missions) { }
@@ -182,6 +180,12 @@ namespace GemMatch {
 
         public void OnClickEntity(Entity entity) {
             Controller.Input(Controller.GetTile(entity).Index);
+        }
+
+        private void RedrawEdges() {
+            foreach(var tileView in TileViews) {
+                tileView.RedrawEdges(TileUtility.GetAdjacentTiles, Controller.Tiles);
+            }
         }
 
         internal virtual EntityView CreateEntityView(Entity entity) {
