ca61830bec92fd7b4a2b43a25bce2d5e6b660515
ghyoon <ghyoon@puzzleone.com>
2023-08-10 22:55:13 +0900

    Polish | PlayBooster

--- program/Assets/Scripts/Pages/PlayBoosterUI.cs
+++ program/Assets/Scripts/Pages/PlayBoosterUI.cs
@@ -1,5 +1,15 @@
+using TMPro;
+using UnityEngine;
+
 namespace Pages {
+    public enum PlayBoosterType {
+        Undo, Magnet, Random, Lock, 
+    }
     public class PlayBoosterUI {
-        
+        [SerializeField] private GameObject goIcon;
+        [SerializeField] private GameObject goLock;
+        [SerializeField] private TMP_Text txtUnlockLevel;
+
+
     }
 }
