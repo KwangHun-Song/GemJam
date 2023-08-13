5fc4791bec7272ce5d682a0a64a541b7a32a2aac
ghyoon <ghyoon@puzzleone.com>
2023-08-13 15:58:02 +0900

    tmp

---
+++ program/Assets/Scripts/Pages/CharacterUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Pages {
+    public class CharacterUI : MonoBehaviour {
+        [SerializeField] private Animator gnomeAni;
+        [SerializeField] private Animator doggyAni;
+
+        
+    }
+}
